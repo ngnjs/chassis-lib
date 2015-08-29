4f13b75bef62bc429e0eda819e10d3d114d05866
Corey Butler <corey@coreybutler.com>
2015-08-29 12:20:48 -0500

    Packaged lib for npm deployment, with installer script.

---
+++ _installer.js
@@ -0,0 +1,16 @@
+var fs = require('fs'),
+    path = require('path'),
+    wrench = require('wrench');
+
+var root = path.join(process.cwd(),'chassis');
+
+// Copy files to new directory
+wrench.copyDirSyncRecursive(path.join(__dirname,'lib'), root, {
+  forceDelete: true, // Whether to overwrite existing directory or not
+  excludeHiddenUnix: true, // Whether to copy hidden Unix files or not (preceding .)
+  preserveFiles: false, // If we're overwriting something and the file already exists, keep the existing
+  preserveTimestamps: true, // Preserve the mtime and atime when copying files
+  inflateSymlinks: true // Whether to follow symlinks or not when copying files
+});
+
+require('fs').unlinkSync('./_installer.js')
