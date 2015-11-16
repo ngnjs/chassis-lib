cae5f12ada1fdfcad6ca4759950b375f86647e14
Corey Butler <corey@coreybutler.com>
2015-11-15 18:24:39 -0600

    Updated build.

--- gulpfile.js
+++ gulpfile.js
@@ -9,9 +9,11 @@
 var gulp = require('gulp')
 var concat = require('gulp-concat')
 var uglify = require('gulp-uglify')
+var header = require('gulp-header')
 var del = require('del')
 var fs = require('fs')
 var path = require('path')
+var headerComment = '/**\n  * Generated on: ' + (new Date()) + '\n  * Copyright (c) 2014-' + (new Date()).getFullYear() + ', Corey Butler. All Rights Reserved.\n  */\n'
 
 var DIR = {
   source: path.resolve('./src'),
@@ -41,7 +43,9 @@ gulp.task('copy', function () {
   // Copy individual files
   gulp.src(sources, {
     base: DIR.source
-  }).pipe(gulp.dest(DIR.dist))
+  })
+  .pipe(header(headerComment))
+  .pipe(gulp.dest(DIR.dist))
 
   // Minify each individual file
   sources.forEach(function (file, index) {
@@ -53,12 +57,14 @@ gulp.task('copy', function () {
         warnings: true
       }
     }))
+    .pipe(header(headerComment))
     .pipe(gulp.dest(DIR.dist))
   })
 
   // Generate full project
   gulp.src(sources)
   .pipe(concat('chassis.js'))
+  .pipe(header(headerComment))
   .pipe(gulp.dest(DIR.dist))
 
   return gulp.src(sources)
@@ -69,6 +75,7 @@ gulp.task('copy', function () {
       warnings: true
     }
   }))
+  .pipe(header(headerComment))
   .pipe(gulp.dest(DIR.dist))
 })
 
