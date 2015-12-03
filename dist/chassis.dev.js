edd457df31e5967de48de34b8fdb3c44de3a8144
Corey Butler <corey@coreybutler.com>
2015-12-02 21:08:08 -0600

    Updated distribution

--- dist/chassis.dev.js
+++ dist/chassis.dev.js
@@ -1,5 +1,5 @@
 /**
-  * v1.0.10 generated on: Wed Dec 02 2015 21:06:48 GMT-0600 (CST)
+  * v1.0.10 generated on: Wed Dec 02 2015 21:08:07 GMT-0600 (CST)
   * Copyright (c) 2014-2015, Corey Butler. All Rights Reserved.
   */
 /**
