4bacc6a1f080a3e60011a3e5346a68d80c5d5373
Corey Butler <corey@coreybutler.com>
2015-12-15 16:56:38 -0600

    Revert to SVG replacement that works on all browsers.

--- dist/chassis.dev.js
+++ dist/chassis.dev.js
@@ -1,5 +1,5 @@
 /**
-  * v1.0.11 generated on: Tue Dec 15 2015 16:55:56 GMT-0600 (CST)
+  * v1.0.11 generated on: Tue Dec 15 2015 16:56:39 GMT-0600 (CST)
   * Copyright (c) 2014-2015, Corey Butler. All Rights Reserved.
   */
 /**
@@ -1671,10 +1671,17 @@ Object.defineProperties(window.NGN.HTTP, {
 // Prevent FOUC
 var __preventfouc = function () {
   var ss = document.createElement('style')
-  ss.innerHTML = 'svg[src],svg[id]{display:none;width:0;height:0;}'
+  var str = document.createTextNode('svg[src]{display:none}svg.loading{height:0px !important;width:0px !important}')
+  ss.appendChild(str)
   document.head.appendChild(ss)
 }
 __preventfouc()
+
+// Add support for node-ish environments (Electron, NW.js, etc)
+var _nodeish_env = false
+try {
+  _nodeish_env = require !== undefined
+} catch (e) {}
 
 // SVG Controller
 window.NGN = window.NGN || {}
@@ -1682,6 +1689,52 @@ window.NGN.DOM = window.NGN.DOM || {}
 window.NGN.DOM.svg = {}
 
 Object.defineProperties(window.NGN.DOM.svg, {
+  /**
+   * @property {Object} _cache
+   * A cache of SVG images.
+   */
+  _cache: NGN.define(false, false, true, {}),
+
+  /**
+   * @method swap
+   * Replace image tags with the SVG equivalent.
+   * @param {HTMLElement|NodeList} imgs
+   * The HTML element or node list containing the images that should be swapped out for SVG files.
+   * @param {function} [callback]
+   * Executed when the image swap is complete. There are no arguments passed to the callback.
+   * @private
+   */
+  swap: NGN.define(false, false, false, function (svgs, callback) {
+    var me = this
+    for (var i = 0; i < svgs.length; i++) {
+      var attr = svgs[i].attributes
+      var output = me._cache[svgs[i].getAttribute('src')]
+      var attrs = []
+      try {
+        attrs = /<svg(\s.*=[\"\'].*?[\"\'])?>/i.exec(output)[1].trim()
+        var sep = /[\"\']\s/i.exec(attrs)[0]
+        attrs = attrs.replace(new RegExp(sep, 'gi'), sep.replace(/\s/ig, ',')).split(',')
+      } catch (e) {}
+      var map = attrs.map(function (els) {
+        return els.split('=')[0].trim().toLowerCase()
+      })
+      for (var x = 0; x < attr.length; x++) {
+        var idx = map.indexOf(attr[x].name.toLowerCase())
+        if (idx < 0) {
+          attrs.push(attr[x].name + '="' + attr[x].value + '"')
+        } else {
+          attrs[idx] = attr[x].name + '="' + attr[x].value + '"'
+        }
+      }
+      attrs = attrs.filter(function (a) {
+        return a.split('=')[0].toLowerCase() !== 'src'
+      })
+      var svg = '<svg ' + attrs.join(' ') + '>'
+      svgs[i].outerHTML = output.replace(/<svg.*?>/i, svg)
+    }
+    callback && callback()
+  }),
+
   /**
    * @method id
    * @param  {string} url
@@ -1702,7 +1755,11 @@ Object.defineProperties(window.NGN.DOM.svg, {
    * @private
    */
   cleanCode: NGN.define(false, false, false, function (code) {
-    return code.toString().trim().replace(/(\r\n|\n|\r)/gm, '').replace(/\s+/g, ' ').replace(/(.*<svg.*?>|<\/svg>)/igm, '')
+    try {
+      return code.toString().trim().replace(/(\r\n|\n|\r)/gm, '').replace(/\s+/g, ' ').match(/\<svg.*\<\/svg\>/igm, '')[0]
+    } catch (e) {
+      return ''
+    }
   }),
 
   /**
@@ -1717,74 +1774,17 @@ Object.defineProperties(window.NGN.DOM.svg, {
     return /(viewbox=["'])(.*?)(["'])/igm.exec(code.toString().trim())[2] || '0 0 100 100'
   }),
 
-  /**
-   * @method swap
-   * Replace image tags with the SVG equivalent.
-   * @param {String|Array} urls
-   * The URL(s) of the associated SVG file that should be added to the DOM.
-   * @param {function} [callback]
-   * Executed when the image swap is complete. There are no arguments passed to the callback.
-   * @private
-   */
-  swap: NGN.define(false, false, false, function (urls, callback) {
-    urls = Array.isArray(urls) ? urls : [urls]
-    if (urls.length > 0) {
-      var collection = document.getElementById('ngn-svg-collection')
-      !collection && document.body.insertAdjacentHTML('afterbegin', '<svg id="ngn-svg-collection"/>')
+  cache: NGN.define(false, false, false, function (url, svg) {
+    this._cache[url] = svg
+  }),
 
-      var unprocessed = urls.length
-      var monitor = setInterval(function () {
-        if (unprocessed === 0) {
-          clearInterval(monitor)
-          document.getElementById('ngn-svg-collection').insertAdjacentHTML('beforeend', symbols)
-          callback && callback()
-        }
-      }, 2)
-
-      var symbols = ''
+  fetchFile: NGN.define(false, false, false, function (url, callback) {
+    if (_nodeish_env) {
+      callback && callback(require('fs').readFileSync(url.replace('file://', '')))
+    } else {
       var me = this
-
-      urls.forEach(function (url) {
-        var id = me.id(url)
-        if (document.querySelector('#ngn-svg-collection > symbol#' + id) === null && symbols.indexOf('id="' + id + '"') < 0) {
-          var src = localStorage.getItem(id)
-          if (src) {
-            src = src.split('|')
-            symbols += '<symbol id="' + id + '" viewBox="' + src[0] + '">' + src[1] + '</symbol>'
-            unprocessed--
-          } else {
-            // Add support for node-ish environments (Electron, NW.js, etc)
-            var _module = false
-            try {
-              _module = require !== undefined
-            } catch (e) {}
-
-            if (_module) {
-              try {
-                var raw = require('fs').readFileSync(url.replace('file://', ''))
-                var content = me.cleanCode(raw)
-                var viewbox = me.viewbox(raw)
-                localStorage.setItem(id, viewbox + '|' + content)
-                symbols += '<symbol id="' + id + '" viewBox="' + viewbox + '">' + content + '</symbol>'
-              } catch (e) {
-                console.error(e.stack)
-              }
-              unprocessed--
-            } else {
-              NGN.HTTP.get(url, function (res) {
-                if (res.status === 200) {
-                  var content = me.cleanCode(res.responseText)
-                  var viewbox = me.viewbox(res.responseText)
-                  localStorage.setItem(id, viewbox + '|' + content)
-                  symbols += '<symbol id="' + id + '" viewBox="' + viewbox + '">' + content + '</symbol>'
-                }
-                unprocessed--
-              })
-            }
-          }
-        } else {
-          unprocessed--
-        }
+      NGN.HTTP.get(url, function (res) {
+        callback && callback(res.status !== 200 ? new Error(res.responseText) : me.cleanCode(res.responseText))
       })
     }
   }),
@@ -1815,19 +1815,34 @@ Object.defineProperties(window.NGN.DOM.svg, {
       : [section]
 
     section.forEach(function (sec) {
-      var svgs = sec.querySelectorAll('svg[src]')
+      var imgs = sec.querySelectorAll('svg[src]')
 
       // Loop through images, prime the cache.
-      var srcs = []
-      for (var i = 0; i < svgs.length; i++) {
-        srcs.push(svgs[i].getAttribute('src'))
+      for (var i = 0; i < imgs.length; i++) {
+        me._cache[imgs[i].getAttribute('src')] = me._cache[imgs[i].getAttribute('src')] || null
       }
-      srcs.length > 0 && me.swap(srcs, function () {
-        for (var i = 0; i < svgs.length; i++) {
-          svgs[i].innerHTML = '<use xlink:href="#' + me.id(svgs[i].getAttribute('src')) + '"/>'
-          svgs[i].removeAttribute('src')
-        }
+
+      // Fetch all of the unrecognized svg files
+      var unfetched = Object.keys(me._cache).filter(function (url) {
+        return me._cache[url] === null
       })
+      var remaining = unfetched.length
+      unfetched.forEach(function (url) {
+        me.fetchFile(url, function (content) {
+          if (!(content instanceof Error)) {
+            me.cache(url, content)
+          }
+          remaining--
+        })
+      })
+
+      // Monitor for download completion
+      var monitor = setInterval(function () {
+        if (remaining === 0) {
+          clearInterval(monitor)
+          me.swap(imgs, callback)
+        }
+      }, 5)
     })
   })
 })
