d64f7c25b04441b70757aa6bcb92aae733973ceb
Kevin Moritz <kevin.moritz@level7.io>
2016-06-28 12:04:40 -0500

    tweak sanity checking

--- src/sanity.js
+++ src/sanity.js
@@ -3,6 +3,28 @@
  * The sanity layer adds dummy checks around methods to make sure they are being called correctly throughout the application.
  * It is designed for development, and not meant to be included in production.
  */
+NGN.Sanity = {}
+
+Object.defineProperties(NGN.Sanity, {
+  /**
+   * @method xhr
+   * Issue an XHR request.
+   * @private
+   * @param  {Function} callback
+   * The callback to execute when the request finishes (or times out.)
+   */
+  check: NGN.privateconst(function (type, args) {
+    switch (type) {
+      case 'emit':
+      if (!args.length) {
+        console.warn('emit() called without required args')
+        return false
+      }
+    }
+    return true
+  })
+})
+
 let original = {
   NGN: {
     EventEmitter: {
@@ -10,11 +32,7 @@ let original = {
     }
   }
 }
+
 NGN.EventEmitter.prototype.emit = function () {
-  if (!arguments.length) {
-    console.warn('NGN.BUS.emit called without required args')
-    return
-  }
-  console.log(arguments)
-  original.NGN.EventEmitter.emit.apply(this, arguments)
+  NGN.Sanity.check('emit', arguments) ? original.NGN.EventEmitter.emit.apply(this, arguments) : null
 }
