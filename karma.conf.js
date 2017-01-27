f3e08484b716182547a76750de0d4e1fa19a90b9
Corey Butler <corey@coreybutler.com>
2017-01-27 16:14:19 -0600

    Fixed browser versioning

--- karma.conf.js
+++ karma.conf.js
@@ -61,7 +61,7 @@ switch (mode) {
         customLaunchers['cl_' + browser + '_' + version.toString()] = {
           base: 'SauceLabs',
           browserName: browser,
-          version: version
+          version: version: browser === 'chrome' ? 55 : (browser === 'firefox' ? 50 : version)
         }
       }
     })
